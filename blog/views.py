5b1839e84ad46134bbd4afeae0b113368a3a1316
Koray Biçer <koray.bicer@gmail.com>
2023-03-10 13:00:03 +0000

    Finish logging

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import logging
+from models import Post
 
+
+logger = logging.getLogger(__name__)
 # Create your views here.
+
+def index(request):
+  posts = Post.objects.all()
+  logger.debug("Got %d posts", len(posts))
+
+
+def post_detail(request):
+  if request.POST:
+    
